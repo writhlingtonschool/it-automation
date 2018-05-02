26a6d1937e86dcc36d66912b3bb32dc83223043e
Daniel WILTSHIRE <dwiltshire@wsbe.internal>
2018-05-02 14:43:57 +0100

    added Get-ADInactiveUsers

---
+++ Get-ADInactiveUsers/Get-ADInactiveUsers.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+    This script pulls a report of users who haven't authenticated to on-site PCs in 90 days
+
+.DESCRIPTION
+    This script requires a SMTP server and the Active Directory PowerShell module.
+
+.EXAMPLE
+    Get-InactiveUsers.ps1
+
+.LINK
+    https://github.com/writhlingtonschool/it-automation
+#>
+
+# Variables
+$Days = "90"
+$ReportName = "Auto report: Enabled users with no on-site login within $Days days"
+$SmtpServer = "x"
+$From = "x"
+$To = "x"
+$OU = "x"
+
+# Get users from AD
+$Users = Search-AdAccount -UsersOnly -SearchBase "$OU" -AccountInactive -TimeSpan $Days |
+Where Enabled -eq $True | Select Name, Enabled, LastLogonDate | ConvertTo-Html
+
+# Create the e-mail body
+$Body = "<p>The following users have not logged in on-site in over $Days days.</p>" + $Users
+
+# Send the e-mail
+Send-MailMessage -smtpserver $SmtpServer -from $From -to $To -subject $ReportName -body "$Body" -bodyashtml
--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 #### `Set-ADAttributeFromGLPI`
 
 Synchronises attributes from GLPI to Active Directory.  Requires GLPI REST API.
+
+## Auditing
+
+**Script for auditing.**
+
+#### `Get-ADInactiveUsers`
+
+Gets inactive users (no authentication with x days) and sends the result in an e-mail.
