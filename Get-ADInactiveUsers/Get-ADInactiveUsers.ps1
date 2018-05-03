55eeee9bf33baa3b09d4c98b339fab3ab8c29665
Daniel WILTSHIRE <dwiltshire@wsbe.internal>
2018-05-03 08:59:54 +0100

    added config file logic

--- Get-ADInactiveUsers/Get-ADInactiveUsers.ps1
+++ Get-ADInactiveUsers/Get-ADInactiveUsers.ps1
@@ -12,20 +12,24 @@
     https://github.com/writhlingtonschool/it-automation
 #>
 
+# Import settings from configuration file
+$workDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+[xml]$configFile = Get-Content "$workDir/Get-ADInactiveUsers.xml" -ErrorAction Stop
+
 # Variables
 $Days = "90"
 $ReportName = "Auto report: Enabled users with no on-site login within $Days days"
-$SmtpServer = "x"
-$From = "x"
-$To = "x"
-$OU = "x"
+$SMTPServer = $configFile.Settings.SMTPSettings.SMTPServer
+$SMTPFrom = $configFile.Settings.SMTPSettings.SMTPFrom
+$SMTPTo = $configFile.Settings.SMTPSettings.SMTPTo
+$ADSearchBase = $configFile.Settings.ADSettings.ADSearchBase
 
 # Get users from AD
-$Users = Search-AdAccount -UsersOnly -SearchBase "$OU" -AccountInactive -TimeSpan $Days |
+$Users = Search-AdAccount -UsersOnly -SearchBase "$ADSearchBase" -AccountInactive -TimeSpan $Days |
 Where Enabled -eq $True | Select Name, Enabled, LastLogonDate | ConvertTo-Html
 
 # Create the e-mail body
 $Body = "<p>The following users have not logged in on-site in over $Days days.</p>" + $Users
 
 # Send the e-mail
-Send-MailMessage -smtpserver $SmtpServer -from $From -to $To -subject $ReportName -body "$Body" -bodyashtml
+Send-MailMessage -smtpserver $SMTPServer -from $SMTPFrom -to $SMTPTo -subject $ReportName -body "$Body" -bodyashtml
