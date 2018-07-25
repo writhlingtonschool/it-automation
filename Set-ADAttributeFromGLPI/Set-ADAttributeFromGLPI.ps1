055ad62025fa4bf22527ac73a3131bef4e196369
Daniel WILTSHIRE <dwiltshire@writhlington.org.uk>
2018-07-25 17:56:46 +0100

    Added notification if DryRun is enabled

--- Set-ADAttributeFromGLPI/Set-ADAttributeFromGLPI.ps1
+++ Set-ADAttributeFromGLPI/Set-ADAttributeFromGLPI.ps1
@@ -24,10 +24,10 @@ param
 )
 
 # Enable verbose logging
-if( $Verbose -eq $True )
-{
-    $VerbosePreference = "continue"
-}
+if ( $Verbose -eq $True ) { $VerbosePreference = "continue" }
+
+# Notify if DryRun is true
+if ( $DryRun -eq $True ) { Write-Host "DryRun is true..." }
 
 # Attributes to set ([GLPI API attribute], [AD Attribute], [Valid Input (Regex)])
 $attributes = New-Object System.Collections.ArrayList
