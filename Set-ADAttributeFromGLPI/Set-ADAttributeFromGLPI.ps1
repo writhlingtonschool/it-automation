f7b258b393b2200410cc04397d6459df16c33f31
Daniel WILTSHIRE <dwiltshire@writhlington.org.uk>
2018-07-25 19:58:25 +0100

    modified Verbose handling

--- Set-ADAttributeFromGLPI/Set-ADAttributeFromGLPI.ps1
+++ Set-ADAttributeFromGLPI/Set-ADAttributeFromGLPI.ps1
@@ -25,10 +25,13 @@ param
 )
 
 # Enable verbose logging
-if ( $Verbose -eq $True )
-{
+if ( $Verbose -eq $True ) {
+    Write-Host "Verbose is true..."
     $VerbosePreference = "continue"
 }
+
+# Notify if DryRun is true
+if ( $DryRun -eq $True ) { Write-Host "DryRun is true..." }
 
 # Attributes to set ([GLPI API attribute], [AD Attribute], [Valid Input (Regex)])
 $attributes = New-Object System.Collections.ArrayList
